962228967b7a841e3b2e78c0d0266d0c2978ac54
ahamedasif07 <rxasif31@gmail.com>
2025-01-15 13:27:57 +0600

    buttons desaign

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-
+import { IoMdArrowRoundForward } from "react-icons/io";
 const Hero = () => {
   return (
     <div className="max-w-screen-lg mx-auto">
@@ -13,11 +13,23 @@ const Hero = () => {
   essential!
 </span>
        </h2>
-       <h2 className="text-center mt-[40px] max-w-[600px] mx-auto font-semibold">
+       <h2 className="text-center px-5 mt-[40px] max-w-[600px] mx-auto font-semibold">
         
 We create customized logos with minimalist, timeless designs
 for Solopreneurs, Startups, SaaS and SMEs.
        </h2>
+
+       <div className="flex gap-3 items-center justify-center mt-5">
+       <button className="bg-[#006BFF] text-white px-3 py-2 text-[14px] rounded-lg flex items-center gap-1 hover:scale-105 hover:shadow-lg transition-all duration-300">
+  See Our Offer 
+  <span><IoMdArrowRoundForward/></span>
+</button>
+
+<button className="text-[#006BFF] bg-white px-3 py-2 border border-blue-500 hover:text-white hover:bg-[#006BFF] text-[14px] rounded-lg transition-all duration-300">
+  See Our Offer 
+</button>
+
+       </div>
 
       
           </div>
