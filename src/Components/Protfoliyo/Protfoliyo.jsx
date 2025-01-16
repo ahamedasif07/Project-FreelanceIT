9af66f645d5255763e1b822b57934a19021dc9e2
ahamedasif07 <rxasif31@gmail.com>
2025-01-16 09:26:15 +0600

    protfoliyo texts done

--- src/Components/Protfoliyo/Protfoliyo.jsx
+++ src/Components/Protfoliyo/Protfoliyo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CgProfile } from "react-icons/cg";
 
 
 
@@ -8,9 +9,13 @@ const protfoliyo = () => {
       <div className='flex justify-center'>
         <button className="px-[8px] text-[#006BFF] font-semibold text-[14px] mt-[81px] py-[3px] rounded-full border-[1px] border-blue-500">
           {/* Button content */}
-          <h2>protfoliyo</h2>
+          <h2 className='flex gap-2 items-center'> <span className=' text-[18px] font-semibold'><CgProfile></CgProfile></span> protfoliyo</h2>
         </button>
       </div>
+      <h2 className='text-center text-5xl font-semibold max-w-[600px] mt-[35px] mx-auto'>We don't “just” design,
+      we create impact</h2>
+      <p className='text-[16px] max-w-[880px] mx-auto text-center py-[30px]'>Our aim is to make you more professional, strengthen your authority and set you apart from the competition.
+      Take a look at some of our projects, carried out for customers all over the world, in a variety of sectors.</p>
     </div>
   );
 };
